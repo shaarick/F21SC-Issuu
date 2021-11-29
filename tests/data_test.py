c58405fead350d989a3957413e896a77db7701df
Mohammad Shariq Farooqui <msf2000@hw.ac.uk>
2021-11-29 19:32:53 +0000

    Update data tests

--- tests/data_test.py
+++ tests/data_test.py
@@ -10,6 +10,7 @@ class DataTest(unittest.TestCase):
         self.no_link = ""
         self.random_link = "http://www.google.com"
         self.a = data.get_data(testing=True)
+        self.b = data.get_data_from_url(self.link)
 
     def test_get_data_from_link_invalid_url(self):
         """Test function with empty string"""
@@ -25,7 +26,11 @@ class DataTest(unittest.TestCase):
 
     def test_local_data_shape(self):
         """Test function checking if get_url() converts all JSON objects properly into a df"""
-        self.assertEqual((10003, 28), self.a.shape, "Should be (10003,28)")
+        self.assertEqual((10003, 28), self.a.shape, "Should be (10003, 28)")
+
+    def test_online_data_shape(self):
+        """Test function checking if get_data_from_url() converts all objects properly into a df"""
+        self.assertEqual((102401, 31), self.b.shape, "Should be (102401, 31")
 
 
 if __name__ == '__main__':
